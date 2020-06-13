66550c6dd65626a9a4391cbf51c97f7bada73f38
Victor 古雪冬 <idong1111@gmail.com>
2020-06-13 14:52:59 +0800

    Note action and note-region related

--- Travel-Platform/src/main/java/cn/edu/bitzh/tp/action/NoteAction.java
+++ Travel-Platform/src/main/java/cn/edu/bitzh/tp/action/NoteAction.java
@@ -1,6 +1,8 @@
 package cn.edu.bitzh.tp.action;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -8,8 +10,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 import cn.edu.bitzh.tp.model.Note;
+import cn.edu.bitzh.tp.model.Region;
 import cn.edu.bitzh.tp.service.INoteService;
+import cn.edu.bitzh.tp.service.IRegionService;
 import cn.edu.bitzh.tp.service.impl.NoteService;
+import cn.edu.bitzh.tp.service.impl.RegionService;
 
 /**
  * @author 古学懂_Victor
@@ -18,13 +23,16 @@ import cn.edu.bitzh.tp.service.impl.NoteService;
 public class NoteAction extends ActionSupport {
 	private static final long serialVersionUID = 1L;
 	private ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
-	private INoteService ns = (NoteService) applicationContext.getBean("noteService");
+	private INoteService ns = (NoteService) applicationContext.getBean("noteService");;
+	private ApplicationContext applicationContext2 = new ClassPathXmlApplicationContext("beans.xml");
+	private IRegionService rs = (RegionService) applicationContext2.getBean("regionService");
 	private Note note;
 	private int noteId = 1000;
+	private int regionId = 0;
 
 	public String get() {
 		final Note note = ns.get(this.noteId);
-		//权限判断
+		// 权限判断
 //		if (note.getNotePermission().equals("public")) {
 		this.note = note;
 //		} else {
@@ -36,6 +44,11 @@ public class NoteAction extends ActionSupport {
 	public String insert() {
 		this.note.setNotePostDate(new Date());
 		this.note.setNoteAuthor(1000);
+		Set<Region> regions = new HashSet<Region>();
+		Region region = rs.get(regionId);
+//		region.setId(regionId);
+		regions.add(region);
+		this.note.setRegions(regions);
 		int id = this.ns.insert(this.note);
 		if (id != 0) {
 			this.noteId = id;
@@ -43,6 +56,14 @@ public class NoteAction extends ActionSupport {
 			return ActionSupport.INPUT;
 		}
 		return ActionSupport.SUCCESS;
+	}
+
+	public int getRegionId() {
+		return regionId;
+	}
+
+	public void setRegionId(int regionId) {
+		this.regionId = regionId;
 	}
 
 	public int getNoteId() {
--- Travel-Platform/src/test/java/dao/SpringSessionTest.java
+++ Travel-Platform/src/test/java/dao/SpringSessionTest.java
@@ -1,6 +1,8 @@
 package dao;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -11,8 +13,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import cn.edu.bitzh.tp.model.Note;
 import cn.edu.bitzh.tp.model.NoteDtl;
+import cn.edu.bitzh.tp.model.Region;
 import cn.edu.bitzh.tp.service.INoteService;
+import cn.edu.bitzh.tp.service.IRegionService;
 import cn.edu.bitzh.tp.service.impl.NoteService;
+import cn.edu.bitzh.tp.service.impl.RegionService;
 
 /**
  * @author 古学懂_Victor
@@ -27,23 +32,28 @@ class SpringSessionTest {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
 		INoteService noteService = (NoteService) ctx.getBean("noteService");
 		Note note = null;
-		note = noteService.get(1011);
+		note = noteService.get(1085);
 		System.out.println(note.getNoteDtl().getNoteHeader());
 	}
 
 	@Test
 	void testInsert() {
 		System.out.println("JUnit Test: Hibernate+Spring Session Test Insert");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+		INoteService noteService = (NoteService) ctx.getBean("noteService");
+		ctx = new ClassPathXmlApplicationContext("beans.xml");
+		IRegionService regionService = (RegionService) ctx.getBean("regionService");
 		Note note = new Note();
 		NoteDtl noteDtl = new NoteDtl();
+		Set<Region> regions = new HashSet<Region>();
+		regions.add(regionService.get(265));
 		note.setNoteAuthor(1000);
 		note.setNotePermission("private");
 		note.setNotePostDate(new Date());
+		note.setRegions(regions);
 		noteDtl.setNoteHeader("Test");
 //		noteDtl.setNote(note);
 		note.setNoteDtl(noteDtl);
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-		INoteService noteService = (NoteService) ctx.getBean("noteService");
 		System.out.println(noteService.insert(note));
 	}
 
